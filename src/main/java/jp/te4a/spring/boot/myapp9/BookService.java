4068ceff993bf003a37e766eac7df0077e5453a0
190748 <190748@e0601-07.JC-21.local>
2022-05-25 11:05:23 +0900

    kadai9初回commi

--- src/main/java/jp/te4a/spring/boot/myapp9/BookService.java
+++ src/main/java/jp/te4a/spring/boot/myapp9/BookService.java
@@ -1,6 +1,8 @@
-package jp.te4a.spring.boot.myapp8;
+package jp.te4a.spring.boot.myapp9;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,31 +11,36 @@ public class BookService {
   @Autowired
   BookRepository bookRepository;
   public BookForm create(BookForm bookForm) {
-    bookForm.setId(bookRepository.getBookId());
-    BookBean bookBean = new BookBean();
-    BeanUtils.copyProperties(bookForm, bookBean);
-    bookRepository.create(bookBean);
-    return bookForm;
-  }
-  public BookForm update(BookForm bookForm) {
-	    BookBean bookBean = new BookBean();
-	    BeanUtils.copyProperties(bookForm, bookBean);
-	    bookRepository.update(bookBean);
-	    return bookForm;
+	  BookBean bookBean = new BookBean();
+	  BeanUtils.copyProperties(bookForm, bookBean);
+	  bookRepository.save(bookBean);
+	  return bookForm;
+	}
+
+	public BookForm update(BookForm bookForm) {
+	  BookBean bookBean = new BookBean();
+	  BeanUtils.copyProperties(bookForm, bookBean);
+	  bookRepository.save(bookBean);
+	  return bookForm;
+	}
+
+	  public void delete(Integer id)
+	  {
+		  bookRepository.deleteById(id);
 	  }
-	  public void delete(Integer id) {  bookRepository.delete(id); }
-	  public List<BookForm> findAll() {
+	  public List<BookForm> findAll() 
+	  {
 	    List<BookBean> beanList = bookRepository.findAll();
 	    List<BookForm> formList = new ArrayList<BookForm>();
 	    for(BookBean bookBean: beanList) {
 	      BookForm bookForm = new BookForm();
 	      BeanUtils.copyProperties(bookBean, bookForm);
 	      formList.add(bookForm);
-	    }
+	  }
 	    return formList;
 	    }
 	  public BookForm findOne(Integer id) {
-	    BookBean bookBean = bookRepository.findOne(id);
+	    Optional<BookBean> bookBean = bookRepository.findById(id);
 	    BookForm bookForm = new BookForm();
 	    BeanUtils.copyProperties(bookBean, bookForm);
 	    return bookForm;

ceb4cae9eb204ad096086ac3e5caeb64aeadfada
190748 <190748@e0601-07.JC-21.local>
2022-05-25 09:43:00 +0900

    更新完了

--- src/main/java/jp/te4a/spring/boot/myapp7/HeloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp7/HeloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HelloController {
+public class HeloController {
     @Autowired
     BookService bookService;
     @RequestMapping("books/list")
